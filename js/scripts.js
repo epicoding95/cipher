ad0951d628ad3934959a2436453fde5f83fac5de
Erik Irgens and Kelar Crisp <erik@>
2019-06-03 16:24:56 -0700

    Complete bonus example and stylize in css and add images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,43 @@
 $(function() {
+
+
+  var userSentence = prompt("Enter a sentence!");
+
+  var firstLastCap = function(str){
+    var firstChar = str.charAt(0).toUpperCase();
+    var lastChar = str.charAt(str.length - 1).toUpperCase();
+    return firstChar + lastChar;
+  }
+
+
+  var firstLastReverse = function(str){
+    var firstChar = str.charAt(0).toUpperCase();
+    var lastChar = str.charAt(str.length - 1).toUpperCase();
+    return lastChar + firstChar;
+  }
+
+
+  alert(firstLastCap(userSentence));
+  alert(firstLastReverse(userSentence));
+
+  var thirdFunction = function(){
+    return userSentence + firstLastCap(userSentence) + firstLastReverse(userSentence);
+  };
+
+  alert(thirdFunction(userSentence));
+
+  var fourthFunction = function(str){
+    var halfLength = (str.length/2 -.1).toFixed();
+    return str.charAt(halfLength) + userSentence + firstLastCap(userSentence) + firstLastReverse(userSentence);
+  }
+
+  alert(fourthFunction(userSentence));
+
+  $(".leftimage").click(function(){
+    alert(userSentence);
+  });
+
+  $(".rightimage").click(function(){
+    alert(fourthFunction(userSentence));
+  });
 });
-
-var userSentence = prompt("Enter a sentence!");
-
-var firstLastCap = function(str){
-  var firstChar = str.charAt(0).toUpperCase();
-  var lastChar = str.charAt(str.length - 1).toUpperCase();
-  return firstChar + lastChar;
-}
-
-
-var firstLastReverse = function(str){
-  var firstChar = str.charAt(0).toUpperCase();
-  var lastChar = str.charAt(str.length - 1).toUpperCase();
-  return lastChar + firstChar;
-}
-
-
-alert(firstLastCap(userSentence));
-alert(firstLastReverse(userSentence));
-
-var thirdFunction = function(){
-  return userSentence + firstLastCap(userSentence) + firstLastReverse(userSentence);
-};
-
-alert(thirdFunction(userSentence));
-
-var fourthFunction = function(str){
-  var halfLength = (str.length/2 -.1).toFixed();
-  return str.charAt(halfLength) + userSentence + firstLastCap(userSentence) + firstLastReverse(userSentence);
-}
-
-alert(fourthFunction(userSentence));
